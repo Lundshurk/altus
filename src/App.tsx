8d17977c89fbfdd050b58ef5d68767de9f91a6c4
Ronin Marella <redsamurai828@gmail.com>
2023-12-12 15:07:29 -0600

    edits and such

--- src/App.tsx
+++ src/App.tsx
@@ -147,7 +147,7 @@ function App() {
       </div>
       <div style={{height: '87%', display: 'flex'}}>
         <div className='history'>
-        <input placeholder='Your Name' onChange={(e) => setUsername(e.target.value)} value={username}></input>
+        <input placeholder='Insert your Name' onChange={(e) => setUsername(e.target.value)} value={username}></input>
           <div className='history_message' onClick={() => setUuid("create_new")}>
             Create New
           </div>
@@ -183,7 +183,7 @@ function App() {
           <div className='textfield'>
             <WhiteTextField
               id="outlined-multiline-flexible"
-              placeholder="Prompt..."
+              placeholder="Ask a question..."
               onChange={(e: any) => {
                 settextfield(e.target.value) 
               }}
